ddba5c8fff7e2d931a9415a361070be1864af1b6
timi <timi@mail.staritgp.com>
2019-03-12 21:39:16 +0800

    发帖页面添加图片

--- timi_h5/src/components/m/bbs/posting.vue
+++ timi_h5/src/components/m/bbs/posting.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="margin-left: 0.5rem; margin-right: 0.5rem">
+    <div style="margin-left: 0.5rem; margin-right: 0.5rem; ">
       <header style="width: 100%; margin-top: 0.5rem; margin-bottom: 0.5rem">
         <i class="aui-iconfont aui-icon-close" style="position: fixed; top: 0.5rem; left: 0.5rem; font-size: 20px"></i>
         <div style="text-align: center; font-size: 16px">发布帖子到婷迷论坛</div>
@@ -9,16 +9,28 @@
         <div id="context" contenteditable="true" style="height: auto; min-height: 5rem" @focus="hideDefault" @blur="showDefauls">
           <div></div>
           <span v-if="isShowDefauls" style="position: absolute ;color: rgb(217, 217, 217)">来吧，分享你与依婷的故事…</span>
-        </div><div style="border: rgb(217, 217, 217) dashed 1px;
-                          width: 5rem;
-                          height: 5rem;
-                          line-height: 5rem;
-                          text-align: center;
-                          font-size: 5rem;
-                          color: rgb(217, 217, 217);
-                          font-weight: 200;">
-        +
         </div>
+        <div class="aui-card-list-content">
+          <div class="aui-row aui-row-padded">
+            <div v-for="img in imgs" class="aui-col-xs-4" style="height: 115px">
+              <img class="img-list" :src="img">
+            </div>
+            <div class="aui-col-xs-4" @click="addImg">
+              <img src="http://timizhuo.cn/add_img.png">
+            </div>
+            <!--<div @click="addImg" class="aui-col-xs-4" style="border: rgb(217, 217, 217) dashed 1px;-->
+                          <!--width: 10rem;-->
+                          <!--height: 10rem;-->
+                          <!--line-height: 10rem;-->
+                          <!--text-align: center;-->
+                          <!--font-size: 10rem;-->
+                          <!--color: rgb(217, 217, 217);-->
+                          <!--font-weight: 100;">-->
+              <!--+-->
+            <!--</div>-->
+          </div>
+        </div>
+        <input type="file" id="change" accept="image" @change="change" style="display: none">
       </div>
     </div>
 </template>
@@ -28,7 +40,11 @@
     name: 'posting',
     data () {
       return {
-        isShowDefauls: true
+        isShowDefauls: true,
+        picValue: '',
+        url: '',
+        imgs: [
+        ]
       }
     },
     methods: {
@@ -40,11 +56,35 @@
         if (context == null || context == '') {
           this.isShowDefauls = true;
         }
+      },
+      addImg: function () {
+        document.getElementById('change').click();
+      },
+      change (e) {
+        let files = e.target.files || e.dataTransfer.files;
+        if (!files.length) return;
+        this.picValue = files[0];
+        this.imgs.push(this.getObjectURL(this.picValue))
+      },
+      getObjectURL (file) {
+        var url = null ;
+        if (window.createObjectURL!=undefined) { // basic
+          url = window.createObjectURL(file) ;
+        } else if (window.URL!=undefined) { // mozilla(firefox)
+          url = window.URL.createObjectURL(file) ;
+        } else if (window.webkitURL!=undefined) { // webkit or chrome
+          url = window.webkitURL.createObjectURL(file) ;
+        }
+        return url ;
       }
     }
   }
 </script>
 
 <style scoped>
-
+  .img-list {
+    width:100%;
+    height: 100%;
+    object-fit: cover;
+  }
 </style>
