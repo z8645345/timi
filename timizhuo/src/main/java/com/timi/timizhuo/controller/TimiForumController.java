44c96380b21f279abd51f4aca0995f063eecd807
xuhaoyong_vendor <xuhaoyong_vendor@sensetime.com>
2019-03-18 21:46:43 +0800

    论坛 查询接口 日志

--- timizhuo/src/main/java/com/timi/timizhuo/controller/TimiForumController.java
+++ timizhuo/src/main/java/com/timi/timizhuo/controller/TimiForumController.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.PageInfo;
 import com.timi.timizhuo.common.Constant;
 import com.timi.timizhuo.common.ResponseData;
 import com.timi.timizhuo.dto.TimiForumDto;
-import com.timi.timizhuo.dto.TimiImagesDto;
 import com.timi.timizhuo.dto.TimiUserDto;
 import com.timi.timizhuo.service.TimiForumService;
 import jodd.util.StringUtil;
@@ -18,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+/**
+ * 论坛中心
+ * @author  Cruisin
+ */
 @RestController
 @RequestMapping("/forum")
 @Slf4j
@@ -75,6 +78,7 @@ public class TimiForumController extends  BaseController{
      */
     @PostMapping("/findForum")
     public ResponseData findForum(@RequestBody TimiForumDto timiForumDto) {
+        log.info("forum findForum  request :{}",timiForumDto);
         ResponseData responseData = new ResponseData();
         try {
             PageInfo<TimiForumDto> pageInfo = this.timiForumService.findPage(timiForumDto);
