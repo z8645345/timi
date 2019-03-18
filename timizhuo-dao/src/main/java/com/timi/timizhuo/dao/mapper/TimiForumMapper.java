646ac2db0b9731378d3d7ff5560192a782e3b2cd
xuhaoyong_vendor <xuhaoyong_vendor@sensetime.com>
2019-03-18 17:42:58 +0800

    论坛mapper

--- timizhuo-dao/src/main/java/com/timi/timizhuo/dao/mapper/TimiForumMapper.java
+++ timizhuo-dao/src/main/java/com/timi/timizhuo/dao/mapper/TimiForumMapper.java
@@ -1,9 +1,6 @@
 package com.timi.timizhuo.dao.mapper;
 
-import com.timi.timizhuo.dao.model.TimiColumn;
 import com.timi.timizhuo.dao.model.TimiForum;
-
-import java.util.List;
 
 public interface TimiForumMapper {
 
